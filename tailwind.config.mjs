088d2f8f31588c9eebc2af0293027f125bc6beb6
Sabalan Alaeiyeh <s.alaeiyeh@gmail.com>
2025-02-20 11:53:19 +0100

    init

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
+
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: {
+    files: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    extract
+  },
   theme: {
+    screens,
+    fontSize,
     extend: {
+      screens: {
+        xs: '20rem'
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: 
+  [
+    fluid
+  ],
 };
